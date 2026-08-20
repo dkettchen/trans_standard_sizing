b851e6ddb8c187372288510c9451665177cd1ae8
unknown <dkettchen@gmail.com>
2026-08-20 15:56:38 +0100

    add util to get ratio to height

---
+++ code_folder/utils/measurements_to_height_ratios.py
@@ -0,0 +1,6 @@
+def height_ratio(measurement:float, height:float):
+    """
+    returns the measurement to height ratio 
+    (ex. 30 to 180 would return a ratio of 1/6 or ⁓0.167)
+    """
+    return measurement / height
